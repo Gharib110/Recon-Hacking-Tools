82b5cedab543e078aea619ea6778e9e7a94a6f57
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-04 22:20:22 +0330

    structs of pdf.go metadata parser is written

--- BingMetadataParser/parser/pdf.go
+++ BingMetadataParser/parser/pdf.go
@@ -1,1 +1,33 @@
 package parser
+
+import (
+	"encoding/xml"
+	"regexp"
+)
+
+var refRegex = regexp.MustCompile(`[0-9]+ [0-9]+ R`)
+
+type PDFBytes []byte
+
+type Reference struct {
+	ObjectID int
+	GenID    int
+}
+
+type Info struct {
+	XMLName  xml.Name `xml:"xmpmeta"`
+	Author   string   `xml:"RDF>Description>creator"`
+	Creator  string   `xml:"RDF>Description>CreatorTool"`
+	Producer string   `xml:"RDF>Description>Producer"`
+}
+
+type XRefObject struct {
+	ObjectID int
+	Offset   int64
+}
+
+type XRef struct {
+	StartID   int
+	Count     int
+	ObjectRef []XRefObject
+}
