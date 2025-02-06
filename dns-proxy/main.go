7bec5d9222f55ea12bf601d36110d338027ac0e2
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-06 22:34:06 +0330

    dns proxy functionality is implemented

--- dns-proxy/main.go
+++ dns-proxy/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/miekg/dns"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"sync"
+	"syscall"
 )
 
 func parse(filename string) (map[string]string, error) {
@@ -34,5 +38,63 @@ func parse(filename string) (map[string]string, error) {
 }
 
 func main() {
+	records, err := parse("proxy.config")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	var recordLock sync.RWMutex
+	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+		if len(r.Question) < 1 {
+			dns.HandleFailed(w, r)
+			return
+		}
+
+		name := r.Question[0].Name
+		parts := strings.Split(name, ".")
+		if len(parts) > 1 {
+			name = strings.Join(parts[len(parts)-2:], ".")
+		}
+		recordLock.RLock()
+		match, ok := records[name]
+		recordLock.RUnlock()
+
+		if !ok {
+			dns.HandleFailed(w, r)
+			return
+		}
+		respp, err := dns.Exchange(r, match)
+		if err != nil {
+			dns.HandleFailed(w, r)
+			return
+		}
+		err = w.WriteMsg(respp)
+		if err != nil {
+			dns.HandleFailed(w, r)
+			return
+		}
+
+	})
+
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGUSR1)
+
+		for sig := range sigs {
+			switch sig {
+			case syscall.SIGUSR1:
+				log.Println("SIGUSR1")
+				recordsUpdate, err := parse("proxy.config")
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					recordLock.Lock()
+					records = recordsUpdate
+					recordLock.Unlock()
+				}
+			}
+		}
+	}()
+
+	log.Fatal(dns.ListenAndServe(":53", "udp", nil))
 }
