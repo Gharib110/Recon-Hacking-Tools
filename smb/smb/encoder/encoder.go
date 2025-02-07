5ec7fc9fe18424d3735f1571d9dd00cfb7cb0190
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-07 21:40:51 +0330

    parseTags and getOffsetByFieldName is implemented

--- smb/smb/encoder/encoder.go
+++ smb/smb/encoder/encoder.go
@@ -1,6 +1,11 @@
 package encoder
 
-import "errors"
+import (
+	"errors"
+	"reflect"
+	"strconv"
+	"strings"
+)
 
 type TagMap struct {
 	m   map[string]interface{}
@@ -49,4 +54,78 @@ func (t TagMap) GetBool(key string) (int, error) {
 	}
 
 	return t.Get(key).(int), nil
+}
+
+func parseTags(sf reflect.StructField) (*TagMap, error) {
+	ret := &TagMap{
+		m:   make(map[string]interface{}),
+		has: make(map[string]bool),
+	}
+	tag := sf.Tag.Get("smb")
+	smbTags := strings.Split(tag, ",")
+	for _, smbTag := range smbTags {
+		tokens := strings.Split(smbTag, ":")
+		switch tokens[0] {
+		case "len", "offset", "count":
+			if len(tokens) != 2 {
+				return nil, errors.New("invalid tag")
+			}
+			ret.Set(tokens[0], tokens[1])
+
+		case "fixed":
+			if len(tokens) != 2 {
+				return nil, errors.New("invalid tag")
+			}
+			i, err := strconv.Atoi(tokens[1])
+			if err != nil {
+				return nil, err
+			}
+			ret.Set(tokens[0], i)
+		case "asn1":
+			ret.Set(tokens[0], true)
+		}
+	}
+	return ret, nil
+}
+
+func getOffsetByFieldName(fieldName string, meta *Metadata) (uint64, error) {
+	if meta == nil || meta.Tags == nil ||
+		meta.Parent == nil || meta.Lens == nil {
+		return 0, errors.New("invalid metadata")
+	}
+	var ret uint64
+	var found bool
+
+	parentvf := reflect.Indirect(reflect.ValueOf(meta.Parent))
+	for i := 0; i < parentvf.NumField(); i++ {
+		field := parentvf.Type().Field(i)
+		if field.Name == fieldName {
+			found = true
+			break
+		}
+		if l, ok := meta.Lens[field.Name]; ok {
+			ret += l
+		} else {
+			buf, err := Marshal(parentvf.Field(i).Interface())
+			if err != nil {
+				return 0, err
+			}
+			l := uint64(len(buf))
+			meta.Lens[field.Name] = l
+			ret += l
+		}
+
+	}
+	if !found {
+		return 0, errors.New("field not found")
+	}
+	return ret, nil
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	return marshal(v, nil)
+}
+
+func marshal(v interface{}, meta *Metadata) ([]byte, error) {
+	return nil, nil
 }
