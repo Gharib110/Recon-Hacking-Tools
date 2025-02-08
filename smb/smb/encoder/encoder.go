afd0e5d6393ac63bba5730a3066ce09d47e7afb5
Alireza Gharib <alirezagharib110@gmail.com>
2025-02-08 23:19:47 +0330

    marshal and getFieldLengthByName is implemented

--- smb/smb/encoder/encoder.go
+++ smb/smb/encoder/encoder.go
@@ -1,7 +1,10 @@
 package encoder
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
+	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
@@ -126,6 +129,187 @@ func Marshal(v interface{}) ([]byte, error) {
 	return marshal(v, nil)
 }
 
+func getFieldLengthByName(fieldName string, meta *Metadata) (uint64, error) {
+	var ret uint64
+	if meta == nil || meta.Tags == nil || meta.Parent == nil || meta.Lens == nil {
+		return 0, errors.New("Cannot determine field length. Missing required metadata")
+	}
+
+	// Check if length is stored in field length cache
+	if val, ok := meta.Lens[fieldName]; ok {
+		return uint64(val), nil
+	}
+
+	parentvf := reflect.Indirect(reflect.ValueOf(meta.Parent))
+
+	field := parentvf.FieldByName(fieldName)
+	if !field.IsValid() {
+		return 0, errors.New("Invalid field. Cannot determine length.")
+	}
+
+	bm, ok := field.Interface().(BinaryMarshall)
+	if ok {
+		// Custom marshallable interface found.
+		buf, err := bm.(BinaryMarshall).MarshalBinary(meta)
+		if err != nil {
+			return 0, err
+		}
+		return uint64(len(buf)), nil
+	}
+
+	if field.Kind() == reflect.Ptr {
+		field = field.Elem()
+	}
+
+	switch field.Kind() {
+	case reflect.Struct:
+		buf, err := Marshal(field.Interface())
+		if err != nil {
+			return 0, err
+		}
+		ret = uint64(len(buf))
+	case reflect.Interface:
+		return 0, errors.New("Interface length calculation not implemented")
+	case reflect.Slice, reflect.Array:
+		switch field.Type().Elem().Kind() {
+		case reflect.Uint8:
+			ret = uint64(len(field.Interface().([]byte)))
+		default:
+			return 0, errors.New("Cannot calculate the length of unknown slice type for " + fieldName)
+		}
+	case reflect.Uint8:
+		ret = uint64(binary.Size(field.Interface().(uint8)))
+	case reflect.Uint16:
+		ret = uint64(binary.Size(field.Interface().(uint16)))
+	case reflect.Uint32:
+		ret = uint64(binary.Size(field.Interface().(uint32)))
+	case reflect.Uint64:
+		ret = uint64(binary.Size(field.Interface().(uint64)))
+	default:
+		return 0, errors.New("Cannot calculate the length of unknown kind for field " + fieldName)
+	}
+	meta.Lens[fieldName] = ret
+	return ret, nil
+}
+
 func marshal(v interface{}, meta *Metadata) ([]byte, error) {
-	return nil, nil
+	var ret []byte
+	typev := reflect.TypeOf(v)
+	valuev := reflect.ValueOf(v)
+
+	bm, ok := v.(BinaryMarshall)
+	if ok {
+		// Custom marshallable interface found.
+		buf, err := bm.MarshalBinary(meta)
+		if err != nil {
+			return nil, err
+		}
+		return buf, nil
+	}
+
+	if typev.Kind() == reflect.Ptr {
+		valuev = reflect.Indirect(reflect.ValueOf(v))
+		typev = valuev.Type()
+	}
+
+	w := bytes.NewBuffer(ret)
+	switch typev.Kind() {
+	case reflect.Struct:
+		m := &Metadata{
+			Tags:   &TagMap{},
+			Lens:   make(map[string]uint64),
+			Parent: v,
+		}
+		for j := 0; j < valuev.NumField(); j++ {
+			tags, err := parseTags(typev.Field(j))
+			if err != nil {
+				return nil, err
+			}
+			m.Tags = tags
+			buf, err := marshal(valuev.Field(j).Interface(), m)
+			if err != nil {
+				return nil, err
+			}
+			m.Lens[typev.Field(j).Name] = uint64(len(buf))
+			if err := binary.Write(w, binary.LittleEndian, buf); err != nil {
+				return nil, err
+			}
+		}
+	case reflect.Slice, reflect.Array:
+		switch typev.Elem().Kind() {
+		case reflect.Uint8:
+			if err := binary.Write(w, binary.LittleEndian, v.([]uint8)); err != nil {
+				return nil, err
+			}
+		case reflect.Uint16:
+			if err := binary.Write(w, binary.LittleEndian, v.([]uint16)); err != nil {
+				return nil, err
+			}
+		}
+	case reflect.Uint8:
+		if err := binary.Write(w, binary.LittleEndian, valuev.Interface().(uint8)); err != nil {
+			return nil, err
+		}
+	case reflect.Uint16:
+		data := valuev.Interface().(uint16)
+		if meta != nil && meta.Tags.Has("len") {
+			fieldName, err := meta.Tags.GetString("len")
+			if err != nil {
+				return nil, err
+			}
+			l, err := getFieldLengthByName(fieldName, meta)
+			if err != nil {
+				return nil, err
+			}
+			data = uint16(l)
+		}
+		if meta != nil && meta.Tags.Has("offset") {
+			fieldName, err := meta.Tags.GetString("offset")
+			if err != nil {
+				return nil, err
+			}
+			l, err := getOffsetByFieldName(fieldName, meta)
+			if err != nil {
+				return nil, err
+			}
+			data = uint16(l)
+		}
+		if err := binary.Write(w, binary.LittleEndian, data); err != nil {
+			return nil, err
+		}
+	case reflect.Uint32:
+		data := valuev.Interface().(uint32)
+		if meta != nil && meta.Tags.Has("len") {
+			fieldName, err := meta.Tags.GetString("len")
+			if err != nil {
+				return nil, err
+			}
+			l, err := getFieldLengthByName(fieldName, meta)
+			if err != nil {
+				return nil, err
+			}
+			data = uint32(l)
+		}
+		if meta != nil && meta.Tags.Has("offset") {
+			fieldName, err := meta.Tags.GetString("offset")
+			if err != nil {
+				return nil, err
+			}
+			l, err := getOffsetByFieldName(fieldName, meta)
+			if err != nil {
+				return nil, err
+			}
+			data = uint32(l)
+		}
+		if err := binary.Write(w, binary.LittleEndian, data); err != nil {
+			return nil, err
+		}
+	case reflect.Uint64:
+		if err := binary.Write(w, binary.LittleEndian, valuev.Interface().(uint64)); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, errors.New(fmt.Sprintf("Marshal not implemented for kind: %s", typev.Kind()))
+	}
+	return w.Bytes(), nil
 }
