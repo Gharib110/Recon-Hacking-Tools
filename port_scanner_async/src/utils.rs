47259b93ed4820a5cc5fa21fb05aad3739efe4bf
Alireza Gharib <alirezagharib110@gmail.com>
2025-03-27 00:28:16 +0330

    enumerate is converted to use tokio async library

---
+++ port_scanner_async/src/utils.rs
@@ -0,0 +1,111 @@
+use thiserror::Error;
+use serde::Deserialize;
+use futures::stream;
+use futures::StreamExt;
+use reqwest::Client;
+use std::{collections::HashSet, time::Duration};
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    name_server::{GenericConnection, GenericConnectionProvider, TokioRuntime},
+    AsyncResolver,
+};
+use std::net::{SocketAddr, ToSocketAddrs};
+use tokio::net::TcpStream;
+use tokio::sync::mpsc;
+
+#[derive(Debug, Clone)]
+pub struct Subdomain {
+    pub domain: String,
+    pub open_ports: Vec<Port>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Port {
+    pub port: u16,
+    pub is_open: bool,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CrtShEntry {
+    pub name_value: String,
+}
+
+type DnsResolver = AsyncResolver<GenericConnection, GenericConnectionProvider<TokioRuntime>>;
+
+pub const MOST_COMMON_PORTS: &[u16] = &[
+    80, 23, 443, 21, 22, 25
+];
+
+
+#[derive(Error, Debug, Clone)]
+pub enum Error {
+    #[error("Usage: port_scanner_async <domain>")]
+    CliUsage,
+    #[error("Request: {0}")]
+    Request(String),
+}
+
+impl std::convert::From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::Request(err.to_string())
+    }
+}
+
+
+pub async fn enumerate(http_client: &Client, target: &str) -> Result<Vec<Subdomain>, Error> {
+    let entries: Vec<CrtShEntry> = http_client
+        .get(&format!("https://crt.sh/?q=%25.{}&output=json", target))
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    let mut dns_resolver_opts = ResolverOpts::default();
+    dns_resolver_opts.timeout = Duration::from_secs(4);
+
+    let dns_resolver = AsyncResolver::tokio(
+        ResolverConfig::default(),
+        dns_resolver_opts,
+    )
+        .expect("subdomain resolver: building DNS client");
+
+    // clean and dedup results
+    let mut subdomains: HashSet<String> = entries
+        .into_iter()
+        .map(|entry| {
+            entry
+                .name_value
+                .split("\n")
+                .map(|subdomain| subdomain.trim().to_string())
+                .collect::<Vec<String>>()
+        })
+        .flatten()
+        .filter(|subdomain: &String| subdomain != target)
+        .filter(|subdomain: &String| !subdomain.contains("*"))
+        .collect();
+    subdomains.insert(target.to_string());
+
+    let subdomains: Vec<Subdomain> = stream::iter(subdomains.into_iter())
+        .map(|domain| Subdomain {
+            domain,
+            open_ports: Vec::new(),
+        })
+        .filter_map(|subdomain| {
+            let dns_resolver = dns_resolver.clone();
+            async move {
+                if resolves(&dns_resolver, &subdomain).await {
+                    Some(subdomain)
+                } else {
+                    None
+                }
+            }
+        })
+        .collect()
+        .await;
+
+    Ok(subdomains)
+}
+
+pub async fn resolves(dns_resolver: &DnsResolver, domain: &Subdomain) -> bool {
+    dns_resolver.lookup_ip(domain.domain.as_str()).await.is_ok()
+}
